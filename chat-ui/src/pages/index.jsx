c2cdb5956b4c3eeac53475bfe6f3f5ab7f1eaced
joemerrillis <joemerrillis@gmail.com>
2025-07-20 14:21:29 -0400

    Update index.jsx

--- chat-ui/src/pages/index.jsx
+++ chat-ui/src/pages/index.jsx
@@ -2,19 +2,35 @@ import React, { useState } from 'react';
 import FileTreeSelector from '../components/FileTreeSelector';
 import ChatWindow from '../components/ChatWindow';
 import LogViewer from '../components/LogViewer';
+import SessionSelector from '../components/SessionSelector';
 
 export default function MainLayout() {
   const [primedFiles, setPrimedFiles] = useState([]);
+  const [currentSession, setCurrentSession] = useState(null);
 
   return (
-    <div className="grid grid-cols-3 h-screen">
-      <div className="border-r p-2 overflow-auto">
+    <div className="grid grid-cols-12 h-screen bg-white text-gray-900">
+      
+      {/* Sidebar - Left */}
+      <div className="col-span-3 p-4 border-r overflow-y-auto">
+        <SessionSelector
+          currentSession={currentSession}
+          setCurrentSession={setCurrentSession}
+        />
         <FileTreeSelector onPrime={setPrimedFiles} />
       </div>
-      <div className="col-span-1.5 p-4">
-        <ChatWindow contextFiles={primedFiles} />
+
+      {/* Main Chat - Center */}
+      <div className="col-span-6 p-4 flex flex-col">
+        <h2 className="text-xl font-semibold mb-2">🧠 AI Chat</h2>
+        <ChatWindow
+          contextFiles={primedFiles}
+          currentSession={currentSession}
+        />
       </div>
-      <div className="border-l p-2 overflow-auto">
+
+      {/* Logs - Right */}
+      <div className="col-span-3 p-4 border-l overflow-y-auto">
         <LogViewer />
       </div>
     </div>
