c2c4bab7debdc27d3df1b79b142274c3e63e04c0
joemerrillis <joemerrillis@gmail.com>
2025-10-23 21:50:11 -0400

    Delete chat-api directory

--- chat-api/src/db/queries.js
+++
@@ -1,9 +0,0 @@
-import { supabase } from './supabase.js';
-
-export async function upsertChunk({ content, embedding, metadata }) {
-  return supabase.from('plugin_chunks').upsert({
-    content,
-    embedding,
-    metadata,
-  });
-}
